6fc8d5ff2f6bb8be4d23da86817d377f395bd88a
Becca Aaronson <becca.aaronson@gmail.com>
2014-08-05 13:30:32 -0500

    git init

---
+++ app/scripts/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$(".story-wrapper").fitVids();
+
+  $('.flexslider').flexslider({
+    slideshow: false,
+    prevText: " ",
+    nextText: " "
+  });
+  $('.flexslider').fitVids();
+
+});
